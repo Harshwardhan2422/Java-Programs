e7bd860d0263772e1f1849d61dbcf0e70cce8174
Harshwardhan2422 <harshwardhanborale@gmail.com>
2026-03-17 19:50:47 +0530

    Delete Basic-Programs directory

--- Basic-Programs/Program1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-/*
-    step1 : Understand the problem statement 
-    step2 : Write the Algorithm 
-    step3 : Decide the Programming language 
-    step4 : Write the program 
-    step5 : Test the program 
-*/
